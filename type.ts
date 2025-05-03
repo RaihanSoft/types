f80bf7a9b9e78cdc5dd414e06384e2bc1cfb7b7d
Raihan <aboraihan14@gmail.com>
2025-05-03 14:40:01 +0600

    type assertion

---
+++ type.ts
@@ -0,0 +1,33 @@
+let anything: any;
+anything = "Next Level Web Development";
+anything = 12344;
+
+(anything as number).toFixed(2)
+
+
+const KgToGm = (para: string | number): string | number | undefined => {
+    if (typeof (para) === 'string') {
+        let number = parseFloat(para) * 1000;
+        return number
+
+    }
+    if (typeof (para) === "number") {
+        return para * 1000;
+    }
+
+}
+const result1 = KgToGm(1000) as number
+const result2 = KgToGm("1000") as string
+console.log(result1)
+console.log(result2)
+
+
+type customeError = {
+    message : string;
+}
+try{
+
+}catch(error){
+    console.log((error as customeError).message)
+
+}
